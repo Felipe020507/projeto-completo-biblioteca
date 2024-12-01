7ca32adb593718ca397b88a81031d0d7221b9df0
Felipe de Souza Casale <129906646+Felipe020507@users.noreply.github.com>
2024-12-01 12:08:18 -0300

    Update biblioteca.sql

--- banco/biblioteca.sql
+++ banco/biblioteca.sql
@@ -4,12 +4,12 @@ USE biblioteca_virtual;
 
 -- Criação da tabela de livros
 CREATE TABLE livros (
-    id INT AUTO_INCREMENT PRIMARY KEY,  -- ID único do livro
-    titulo VARCHAR(255) NOT NULL,       -- Título do livro
-    isbn VARCHAR(20),                   -- ISBN do livro (opcional)
-    data_publicacao DATE,               -- Data de publicação do livro
-    data_emprestimo DATE,               -- Data do empréstimo
-    data_devolucao DATE                -- Data da devolução
+    id INT AUTO_INCREMENT PRIMARY KEY,  
+    titulo VARCHAR(255) NOT NULL,       
+    isbn VARCHAR(20),                   
+    data_publicacao DATE,               
+    data_emprestimo DATE,               
+    data_devolucao DATE                
 );
 
 
